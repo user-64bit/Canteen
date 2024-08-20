a16be328a974f9dab8186761a84f4d6a0f86f9ec
us3r64bit <arth.prajapati@outlook.com>
2024-08-20 19:02:00 +0530

    add: model for post,likes,views, etc

--- app/(main)/(protected)/_components/Timeline.tsx
+++ app/(main)/(protected)/_components/Timeline.tsx
@@ -1,9 +1,24 @@
+import { auth } from "@/lib/auth";
 import { Post } from "./Post";
+import { getPostsByUser } from "@/actions/getPostsByUser";
 
-export const Timeline = () => {
+export const Timeline = async () => {
+  const session = await auth();
+  const user = session?.user;
+  const posts = await getPostsByUser({ userId: user?.id! });
   return (
     <div>
-      <Post />
+      {posts?.map((post) => (
+        <Post
+          key={post.id}
+          title={post.title}
+          content={post.content}
+          likes={0}
+          shares={0}
+          views={0}
+          comments={0}
+        />
+      ))}
     </div>
   );
 };
