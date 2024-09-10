e46f1465f7196e830e6fe02248469312429b64d4
user-64bit <arth.prajapati@outlook.com>
2024-09-10 16:05:03 +0530

    fix: logged in user redirects to /community, remove /private/[university]

--- app/(main)/(protected)/private/[university]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default async function MyUniversityLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // don't know if we need this layout or not...
-  return <div className="">{children}</div>;
-}
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Hero } from "@/components/Hero";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function MarketingPage() {
+export default async function MarketingPage() {
+  const session = await auth();
+  if (session?.user) redirect("/community");
   return (
     <>
       <Hero />
