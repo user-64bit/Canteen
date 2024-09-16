072052feda68590a0ce6613da5060199029d67e7
user-64bit <arth.prajapati@outlook.com>
2024-09-16 16:54:13 +0530

    fix

--- app/(main)/(protected)/(sidebar)/all/page.tsx
+++ app/(main)/(protected)/(sidebar)/all/page.tsx
@@ -1,5 +1,5 @@
 export default function AllChannelsPage() {
   return (
-    <div>{"Correntlly I don't know what is the purpose of this page."}</div>
+    <div>{"right now I don't know what is the purpose of this page."}</div>
   );
 }
