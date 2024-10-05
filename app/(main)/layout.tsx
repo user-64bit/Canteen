22af6e4e867a3e385a5890f73867d5af84f7b7d2
user-64bit <arth.prajapati@outlook.com>
2024-10-05 14:34:23 +0530

    fix: theme issue, default is light when user logged in

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { SearchCommnad } from "@/components/SearchCommand";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { setTheme } = useTheme();
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme") ?? "light");
+  });
+
   return (
     <div>
       <SearchCommnad />
